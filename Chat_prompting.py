dc3f79e0fedd062eb4fbcd0b0accfb156d101e8a
Vihaan Gautam <83455954+vihaangautam@users.noreply.github.com>
2025-05-03 22:15:27 +0530

    Update Chat_prompting.py

--- Chat_prompting.py
+++ Chat_prompting.py
@@ -1,1 +1,32 @@
+from google import genai
+from google.genai.types import HttpOptions, ModelContent, Part, UserContent
+
+import logging
+from google.cloud import logging as gcp_logging
+
+# ------  Below cloud logging code is for Qwiklab's internal use, do not edit/remove it. --------
+# Initialize GCP logging
+gcp_logging_client = gcp_logging.Client()
+gcp_logging_client.setup_logging()
+
+client = genai.Client(
+    vertexai=True,
+    project='"project-id"',
+    location='"REGION"',
+    http_options=HttpOptions(api_version="v1")
+)
+chat = client.chats.create(
+    model="gemini-2.0-flash-001",
+    history=[
+        UserContent(parts=[Part(text="Hello")]),
+        ModelContent(
+            parts=[Part(text="Great to meet you. What would you like to know?")],
+        ),
+    ],
+)
+response = chat.send_message("What are all the colors in a rainbow?")
+print(response.text)
+
+response = chat.send_message("Why does it appear when it rains?")
+print(response.text)
 
