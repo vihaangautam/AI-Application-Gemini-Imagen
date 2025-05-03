eae232a2abbe7ec230b033ec408fd6908f749a33
Vihaan Gautam <83455954+vihaangautam@users.noreply.github.com>
2025-05-03 22:12:47 +0530

    Update Image_Recognition.py

--- Image_Recognition.py
+++ Image_Recognition.py
@@ -1,1 +1,17 @@
 
+from google import genai
+from google.genai.types import HttpOptions, Part
+
+client = genai.Client(http_options=HttpOptions(api_version="v1"))
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=[
+        "What is shown in this image?",
+        Part.from_uri(
+            file_uri="gs://cloud-samples-data/generative-ai/image/scones.jpg",
+            mime_type="image/jpeg",
+        ),
+    ],
+)
+print(response.text)
+
